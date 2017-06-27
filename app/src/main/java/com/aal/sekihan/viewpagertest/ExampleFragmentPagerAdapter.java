bc93afdcbb801448d7e2384317c5ff57a4ae6cca
sekihan <akatyann1212@yahooco.jp>
2017-06-27 16:09:59 +0900

    Initial commit

---
+++ app/src/main/java/com/aal/sekihan/viewpagertest/ExampleFragmentPagerAdapter.java
@@ -0,0 +1,47 @@
+package com.aal.sekihan.viewpagertest;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by sekihan on 2017/06/27.
+ */
+
+public  class ExampleFragmentPagerAdapter extends FragmentPagerAdapter {
+    public ExampleFragmentPagerAdapter(FragmentManager fm){
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return ExampleFragment.newInstance(android.R.color.white);
+            case 1:
+                return ExampleFragment.newInstance(android.R.color.white);
+            case 2:
+                return ExampleFragment.newInstance(android.R.color.white);
+        }
+        return null;
+
+    }
+
+    @Override
+    public int getCount(){
+        return 3;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position){
+        switch (position){
+            case 0:
+                return "My時間割";
+            case 1:
+                return "シラバス検索";
+            case 2:
+                return "単位取得状況";
+        }
+        return null;
+    }
+}
