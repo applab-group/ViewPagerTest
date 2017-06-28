c38b88db2eacbe325dac016637eeb05f58066d02
sekihan <akatyann1212@yahooco.jp>
2017-06-28 16:57:04 +0900

    test branch

--- app/src/main/java/com/aal/sekihan/viewpagertest/SyllabusFragment.java
+++ app/src/main/java/com/aal/sekihan/viewpagertest/SyllabusFragment.java
@@ -6,7 +6,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * Created by sekihan on 2017/06/27.
@@ -14,6 +18,7 @@ import android.widget.LinearLayout;
 
 public class SyllabusFragment extends Fragment {
     private final static String BACKGROUND_COLOR = "background_color";
+    ArrayAdapter<String> adapter;
 
     public static SyllabusFragment newInstance(@ColorRes int IdRes){
         SyllabusFragment frag = new SyllabusFragment();
@@ -26,6 +31,7 @@ public class SyllabusFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+
     }
 
     @Override
@@ -33,6 +39,19 @@ public class SyllabusFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_syllabus, null);
         LinearLayout linearLayout = (LinearLayout) view.findViewById(R.id.fragment_main_linearlayout);
         linearLayout.setBackgroundResource(getArguments().getInt(BACKGROUND_COLOR));
+
+        final ArrayList<String> data = new ArrayList<>();
+        data.add("情報数学");
+        data.add("言語解析論");
+        data.add("神保");
+        data.add("胡椒");
+        data.add("胡椒");
+        data.add("胡椒");
+        data.add("胡椒");
+        data.add("胡椒");
+
+        adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, data);
+        ListView list = (ListView) view.findViewById(R.id.syllabusList);
 
         return view;
     }
